a3629f986492191526cf921545799e5ffe2501c9
Miguel-git-test <miguel_iscar@hotmail.com>
2026-04-10 12:42:31 +0200

    actualizacion de cache del service worker para forzar v1.8.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mueve5-v1';
+const CACHE_NAME = 'mueve5-v1.8.3';
 const ASSETS = [
   './',
   './index.html',
@@ -13,6 +13,20 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
